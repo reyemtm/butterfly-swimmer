0c6f7bacc9c593e462d5bc61551008b7393d4e16
Malcolm Meyer <malcolmtmeyer@gmail.com>
2020-10-17 09:02:20 -0400

    Update template.js

--- template.js
+++ template.js
@@ -6,7 +6,7 @@ function generate(content) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ellie's Swim Times | Photos</title>
+    <title>Butterfly Swimmer</title>
     <link href="https://fonts.googleapis.com/css?family=Mukta|Nunito" rel="stylesheet">
     <link rel="stylesheet" href="node_modules/typeface-quicksand/index.css">
     <link rel="stylesheet" href="node_modules/chartist/dist/chartist.min.css">
